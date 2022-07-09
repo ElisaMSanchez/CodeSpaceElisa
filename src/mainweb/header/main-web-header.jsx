1cac39867c5747dcd90a27ed82b2280e1bdbdce8
Elisa <emsp.star@gmail.com>
2022-07-09 13:07:00 +0200

    Añadido boton para ir de main web a admin

--- src/mainweb/header/main-web-header.jsx
+++ src/mainweb/header/main-web-header.jsx
@@ -1,10 +1,17 @@
 import headerLogo from "../image/headerLogo.jpg";
-import './main-web-header.css'
+import './main-web-header.css';
+import {FaRegUserCircle} from "react-icons/fa";
+import {Link} from 'react-router-dom';
 
 export default function MainWebHeader() {
     return (
         <div className="main-web-header">
             <img className="main-web-header-logo" src={headerLogo} alt="logo"/>
+            <div className="main-web-header-login-wrapper">
+                <Link to="/admin">
+                    <FaRegUserCircle className="main-web-header-login" />
+                </Link>
+            </div>
         </div>
     );
 }
